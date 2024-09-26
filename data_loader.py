61899724e5a5b245133d38f773a34baa94d3bc1f
taylorking01 <taylorkinginbox@gmail.com>
2024-09-26 09:00:41 +0000

    Add initial working version of FaceVision project

--- data_loader.py
+++ data_loader.py
@@ -4,13 +4,15 @@ from sklearn.utils import shuffle
 from skimage.transform import resize
 
 def load_data():
-    # Fetch LFW faces dataset
-    lfw_people = fetch_lfw_people(min_faces_per_person=0, resize=1.0, download_if_missing=True)
-    X_faces = lfw_people.images
+    # Fetch LFW faces dataset with resized images
+    lfw_people = fetch_lfw_people(
+        min_faces_per_person=0, resize=0.5, download_if_missing=True
+    )
+    X_faces = lfw_people.images  # Shape: (n_samples_faces, 62, 47)
     y_faces = np.ones(X_faces.shape[0])
 
     # Generate negative samples (e.g., random noise)
-    X_non_faces = np.random.rand(1000, 62, 47)
+    X_non_faces = np.random.rand(1000, 62, 47)  # Match dimensions with X_faces
     y_non_faces = np.zeros(X_non_faces.shape[0])
 
     # Combine datasets
@@ -24,3 +26,4 @@ def load_data():
     X = X / 255.0
 
     return X, y
+
