c08113cefa2ec0ff446ff01c21690d42ca133582
Taylorking01 <taylorkinginbox@gmail.com>
2024-09-28 21:10:57 +0000

    Removed load_images_from_folder from utils.py

--- utils.py
+++ utils.py
@@ -15,17 +15,3 @@ def plot_samples(X, y, rows=2, cols=5):
                 idx += 1
     plt.tight_layout()
     plt.show()
-
-def load_images_from_folder(folder, image_size=(62, 47)):
-    images = []
-    for filename in os.listdir(folder):
-        img_path = os.path.join(folder, filename)
-        try:
-            with Image.open(img_path) as img:
-                img = img.convert('L')  # Convert to grayscale
-                img = img.resize(image_size, Image.ANTIALIAS)
-                img_array = np.array(img, dtype=np.float32) / 255.0
-                images.append(img_array)
-        except Exception as e:
-            print(f'Error loading image {img_path}: {e}')
-    return images
